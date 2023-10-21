2a36cd122f43f594b521e70ee7404b179e08f103
DILLI_DONKA <110531001+DILLIDONKA@users.noreply.github.com>
2023-10-21 12:01:13 +0530

    Create ADDITION.cpp

---
+++ MATHEMATICS/ADDITION.cpp
@@ -0,0 +1,12 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+  int a,b;
+  cout<<"Enter the element a :";
+  cin>>a;
+  cout<<"Enter the element b :";
+  cin>>b;
+  int c=a+b;
+  cout<<"The value of a+b is "<<c;
+  return 0;
+}
